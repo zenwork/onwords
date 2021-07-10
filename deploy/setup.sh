2e1d18e30ba4db927e4348a3289d169d16bf0305
Florian Hehlen <flo@noumenadigital.com>
2021-07-11 00:42:34 +0200

    redesign - prepare new file structure

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -1,12 +1,15 @@
 echo "BUILD FOR PROD"
+
 echo
 echo "--> install latest nodejs "
 node --version
 npm --version
+
 echo
 echo "--> setup onwords"
 cd "$(dirname "$0")" || exit
 cd ../site || (echo "[../site] directory not found" & exit 1)
+
 npm ci
 if npm run build; then
     echo "BUILD COMPLETED"
