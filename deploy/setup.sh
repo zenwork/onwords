26cdb283f0de9dd8b024720b1009ee8104fb7a33
Florian Hehlen <flo@noumenadigital.com>
2021-07-10 21:05:22 +0200

    redesign - prepare new file structure

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -1,5 +1,6 @@
 echo "BUILD FOR PROD"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+nvm install 16
 cd "$(dirname "$0")" || exit
 cd ../site || (echo "[../site] directory not found" & exit 1)
 npm run build
