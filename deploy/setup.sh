feb4a82a4b567c1dc7b96ee032f12cae3599ed12
Florian Hehlen <flo@noumenadigital.com>
2021-07-10 21:20:34 +0200

    redesign - prepare new file structure

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -2,10 +2,16 @@ echo "BUILD FOR PROD"
 echo
 echo "--> install latest nodejs "
 sudo apt update
-sudo apt install --assume-yes nodejs
+sudo apt install --assume-yes nodejs npm
 echo
 echo "--> setup onwords"
 cd "$(dirname "$0")" || exit
 cd ../site || (echo "[../site] directory not found" & exit 1)
-npm run build
-echo "BUILD COMPLETED"
+
+if npm run build; then
+    echo "BUILD COMPLETED"
+else
+    echo "BUILD FAILED";
+    # your termination logic here
+    exit 1;
+fi
