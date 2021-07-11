c84b5dad17c6a422d632977b2d30468cc79161da
Florian Hehlen <flo@noumenadigital.com>
2021-07-12 00:28:53 +0200

    redesign - prepare new file structure

--- deploy/apache/setup.sh
+++ deploy/apache/setup.sh
@@ -1,18 +1,17 @@
 echo 'APACHE 2 CONFIGURE'
 
-
-sudo apt install certbot python3-certbot-apache
-sudo certbot --apache --domain www.onwords.ch --reinstall --redirect
+#sudo apt install certbot python3-certbot-apache
+#sudo certbot --apache --domain www.onwords.ch --reinstall --redirect
 
 ## enable modules
-sudo a2enmod proxy
-sudo a2enmod proxy_http
-sudo a2enmod proxy_balancer
-sudo a2enmod lbmethod_byrequests
+#sudo a2enmod proxy
+#sudo a2enmod proxy_http
+#sudo a2enmod proxy_balancer
+#sudo a2enmod lbmethod_byrequests
 
 ## add configs
-sudo cp Apache2Proxy-ssl.conf /etc/apache2/sites-available/Apache2Proxy-ssl.conf
-sudo cp Apache2Proxy.conf /etc/apache2/sites-available/Apache2Proxy.conf
+sudo cp apache/Apache2Proxy-ssl.conf /etc/apache2/sites-available/Apache2Proxy-ssl.conf
+sudo cp apache/Apache2Proxy.conf /etc/apache2/sites-available/Apache2Proxy.conf
 
 ## remove all sites
 sudo rm /etc/apache2/sites-enabled/*
