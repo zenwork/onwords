e730bdb693a9686a87f0c22bff226aa8ca290fed
zework <florian@hehlen.name>
2023-02-27 01:00:45 +0100

    fixing writing limitation

--- setup.ts
+++ setup.ts
@@ -4,8 +4,8 @@ export async function setup() {
     const env = Deno.env.toObject().ENVIRONMENT
 
     if (env === 'PROD') {
-        await Deno.writeTextFile('./static/index.html',
-            index.replace('<meta content="noindex, nofollow" name="robots">', '<meta content="index, follow" name="robots">'))
+        // await Deno.writeTextFile('./static/index.html',
+        //     index.replace('<meta content="noindex, nofollow" name="robots">', '<meta content="index, follow" name="robots">'))
         console.log('index updated for prod', `[${env}]`)
     } else {
         console.log('index not updated', `[${env}]`)
