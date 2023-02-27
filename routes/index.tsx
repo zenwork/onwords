df615dc767ae8f12df8fdc12244142043615c713
zework <florian@hehlen.name>
2023-02-27 22:51:48 +0100

    added favicon

--- routes/index.tsx
+++ routes/index.tsx
@@ -16,6 +16,11 @@ export default function Home(request: Request) {
     return (
         <>
             <Head>
+                <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png"/>
+                    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png"/>
+                        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png"/>
+                            <link rel="manifest" href="favicon/site.webmanifest"/>
+
                 <meta content="On Words" name="title"/>
                 <meta content="English editing services including copy-editing, proofreading, etc." name="description"/>
                 <meta
